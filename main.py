d8fcd9d4e91afc3d5d4794f5446595260ee4148e
Wouter <w_doppie@hotmail.com>
2020-10-19 23:45:55 +0200

    keras model weights exported to torch model

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
-from craterdetection import CraterUNet
 import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
+import numpy as np
+fig, axes = plt.subplots(2,1)
 
+
+from keras.models import load_model
+deepmoon_model_path = '/Users/doppenberg/Documents/Workspaces/DeepMoon/zenodo_downloads/model_keras2.h5'
+
+deepmoon = load_model(deepmoon_model_path)
+
+img = cv2.imread('data/test_input.png', cv2.IMREAD_GRAYSCALE)
+img = img.reshape(256, 256, 1)
+
+out = deepmoon.predict(img[np.newaxis, :])
+
+axes[0].imshow(out[0], cmap='Greys_r')
+
+from craterdetection import CraterUNet
 import torch
 import torch.nn as nn
 from torchvision import transforms
 
-net = CraterUNet(1, 1)
-net.eval()
+img = cv2.imread('data/test_input.png', cv2.IMREAD_GRAYSCALE)
 
-img = Image.open(r'data/test_input.png').convert('L')
-
-images = transforms.ToTensor()(img).unsqueeze_(0)
+unet = CraterUNet(1, 1)
+for (name, param), w in zip(unet.named_parameters(), deepmoon.weights):
+    if len(param.shape) == 4:
+        param.data = nn.Parameter(torch.Tensor(w.numpy().transpose(3, 2, 1, 0)))
+    else:
+        param.data = nn.Parameter(torch.Tensor(w.numpy()))
+unet.eval()
 
 with torch.no_grad():
-    out = net(images)
+    out = unet(transforms.ToTensor()(img).unsqueeze_(0))
 
-plt.imshow(out.squeeze(), cmap='Greys_r')
+axes[1].imshow(out.squeeze().numpy(), cmap='Greys_r')
+
 plt.show()
