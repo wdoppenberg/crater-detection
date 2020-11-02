32c87390d83055bdee613e1542c230a449f6d1df
Wouter <w_doppie@hotmail.com>
2020-11-02 14:58:14 +0100

    First working Hough transform approach

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 from torchvision import transforms
 
@@ -13,13 +14,21 @@ def main():
     batch = transforms.ToTensor()(img).unsqueeze_(0)
 
     out = exp.infer(batch)
-    extracted_rings = template_match_t(out[0, 0].copy(), minrad=3)
+    # extracted_rings = template_match_t(out[0, 0].copy(), minrad=3)
 
     fig, axes = plt.subplots(1, 2, figsize=(15, 10))
 
-    for x, y, r in extracted_rings:
-        circle = plt.Circle((x, y), r, color='blue', fill=False, linewidth=2, alpha=0.5)
-        axes[0].add_artist(circle)
+    circles = cv2.HoughCircles(np.uint8(out[0, 0].copy()*255), cv2.HOUGH_GRADIENT, 1, 20, None, 160, 25, 1, 18)
+    if circles is not None:
+        # Get the (x, y, r) as integers
+        circles = np.round(circles[0, :]).astype("int")
+
+        # loop over the circles
+        for x, y, r in circles:
+            circle = plt.Circle((x, y), r, color='blue', fill=False, linewidth=2, alpha=0.5)
+            axes[0].add_artist(circle)
+    else:
+        print("No circles found!")
 
     axes[0].imshow(img, cmap='Greys_r')
     axes[1].imshow(out[0, 0], cmap='Greys_r')
